0de92044d50b121353acce976abcb55339c538f0
JeremyWu <Jeremy_Wu@xus-wx.com>
2020-07-03 15:39:04 +0800

    Add a class named MusicBook and add a tag [Export(typeof(IBookService))]

---
+++ Jeremy.MEF/MEF.DemoOne/MusicBook.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MEF.DemoOne
+{
+    [Export(typeof(IBookService))]//将类声明导出为IBookService接口类型
+    public class MusicBook : IBookService
+    {
+        public string BookName { get; set; }
+
+        public string GetBookName()
+        {
+            return "Hi, Music Book!";
+        }
+    }
+}
