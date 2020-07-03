084b316e0a2b831f5ac3a2263be1317c4037e759
JeremyWu <Jeremy_Wu@xus-wx.com>
2020-07-03 15:31:30 +0800

    Add a interface of IBookService

---
+++ Jeremy.MEF/MEF.DemoOne/IBookService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MEF.DemoOne
+{
+    /// <summary>
+    /// 书 接口
+    /// </summary>
+    public interface IBookService
+    {
+        /// <summary>
+        /// 书名
+        /// </summary>
+        string BookName { get; set; }
+
+        /// <summary>
+        /// 获取数据的名字
+        /// </summary>
+        /// <returns></returns>
+        string GetBookName();
+    }
+}
--- Jeremy.MEF/MEF.DemoOne/Program.cs
+++ Jeremy.MEF/MEF.DemoOne/Program.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace MEF.DemoOne
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
