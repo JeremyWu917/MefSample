1db8a0aadf3c7de741f3d3bdc4b4162b6d62a1a5
JeremyWu <Jeremy_Wu@xus-wx.com>
2020-07-03 15:51:06 +0800

    Coding on program and [import] the class and using his function with declare

--- Jeremy.MEF/MEF.DemoOne/Program.cs
+++ Jeremy.MEF/MEF.DemoOne/Program.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.ComponentModel.Composition.Hosting;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,8 +11,27 @@ namespace MEF.DemoOne
 {
     public class Program
     {
+        [Import]//这里使用[Import]导入刚刚导出的MusicBook
+        public IBookService Service { get; set; }
+
         static void Main(string[] args)
         {
+            Program program = new Program();
+            program.Compose();
+            if (program.Service != null)
+            {
+                Console.WriteLine(program.Service.GetBookName());
+            }
+            Console.ReadKey();
+        }
+        /// <summary>
+        /// 实例化CompositionContainer来实现组合
+        /// </summary>
+        private void Compose()
+        {
+            var catalog = new AssemblyCatalog(Assembly.GetExecutingAssembly());
+            CompositionContainer container = new CompositionContainer(catalog);
+            container.ComposeParts(this);
         }
     }
 }
