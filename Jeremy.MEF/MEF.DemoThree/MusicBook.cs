0ef0462c9837db37453b6af42ce7bd0e198fd2ff
JeremyWu <Jeremy_Wu@xus-wx.com>
2020-07-03 16:58:35 +0800

    Add a project to learn the import & export property and function of the class

---
+++ Jeremy.MEF/MEF.DemoThree/MusicBook.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MEF.DemoThree
+{
+    [Export("MusicBook")]//将类以契约名MusicBook的形式导出，不知道类型，默认为Object
+    public class MusicBook : IBookService
+    {
+        //导出私有属性
+        [Export(typeof(string))]
+        private string _privateBookName = "Private Music BookName";
+
+        //导出公有属性
+        [Export(typeof(string))]
+        public string _publicBookName = "Public Music BookName";
+
+
+
+        //导出公有方法
+        [Export(typeof(Func<string>))]
+        public string GetBookName()
+        {
+            return "MusicBook";
+        }
+
+        //导出私有方法
+        [Export(typeof(Func<int, string>))]
+        private string GetBookPrice(int price)
+        {
+            return "$" + price;
+        }
+
+        //---------------------------------------------------------------------------------------------------------------------------------------//
+
+
+
+        public string BookName { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+    }
+}
---
+++ Jeremy.MEF/MEF.DemoThree/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.ComponentModel.Composition.Hosting;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MEF.DemoThree
+{
+    public class Program
+    {
+        //按照契约名导入类，不指定类型的清空下，默认类型为Object，使用时需要强制转换
+        [ImportMany("MusicBook")]
+        public IEnumerable<object> Services { get; set; }
+
+        //导入属性，这里不区分public还是private
+        [ImportMany]
+        public List<string> InputString { get; set; }
+
+        //导入无参数方法
+        [Import]
+        public Func<string> methodWithoutPara { get; set; }
+
+        //导入有参数方法
+        [Import]
+        public Func<int, string> methodWithPara { get; set; }
+
+        static void Main(string[] args)
+        {
+            Program pro = new Program();
+            pro.Compose();
+            Console.WriteLine("----------------------打印导入的类--------------------------------");
+            //打印导入的类
+            if (pro.Services != null)
+            {
+                foreach (var s in pro.Services)
+                {
+                    var ss = (IBookService)s;
+                    Console.WriteLine(ss.GetBookName());
+                }
+            }
+            Console.WriteLine("---------------------打印导入的属性-------------------------------");
+            // 打印导入的属性
+            foreach (var str in pro.InputString)
+            {
+                Console.WriteLine(str);
+            }
+            Console.WriteLine("-------------------打印导入的无参方法-----------------------------");
+            // 调用无参数方法
+            if (pro.methodWithoutPara != null)
+            {
+                Console.WriteLine(pro.methodWithoutPara());
+            }
+            Console.WriteLine("-------------------打印导入的有参方法-----------------------------");
+            // 调用有参数方法
+            if (pro.methodWithPara != null)
+            {
+                Console.WriteLine(pro.methodWithPara(3000));
+            }
+
+
+            Console.ReadKey();
+        }
+
+        private void Compose()
+        {
+            var catalog = new AssemblyCatalog(Assembly.GetExecutingAssembly());
+            CompositionContainer container = new CompositionContainer(catalog);
+            container.ComposeParts(this);
+        }
+    }
+}
